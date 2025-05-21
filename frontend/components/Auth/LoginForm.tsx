41c8156fa82056bbf63339ec0696541b7e64d750
sacazidal <tw1nkle001@yandex.ru>
2025-05-21 23:37:32 +0700

    окно логина с fetch

---
+++ frontend/components/Auth/LoginForm.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { useState } from "react";
+import AuthForm from "./AuthForm";
+import { SwitchFormProps } from "@/types";
+import FieldForm from "../FieldForm";
+
+const LoginForm = ({ onSwitch, onRecovery }: SwitchFormProps) => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setError("");
+    setLoading(true);
+
+    try {
+      const response = await fetch("", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || "Ошибка авторизации");
+      }
+    } catch (error) {
+      console.log(error);
+      setError("Ошибка сервера");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <AuthForm
+      onSubmit={handleSubmit}
+      title="Вход"
+      loading={loading}
+      error={error}
+      action="Войти"
+      secondAction="Входим..."
+      footerText={
+        <>
+          <span>У вас еще нет аккаунта?</span>{" "}
+          <button
+            className="underline underline-offset-4 cursor-pointer"
+            onClick={onSwitch}
+          >
+            Зарегистрироваться
+          </button>
+        </>
+      }
+    >
+      <FieldForm
+        label="Почта"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        type="email"
+        id="email"
+        placeholder="eye@example.com"
+      />
+      <FieldForm
+        label="Пароль"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        id="password"
+        recovery={
+          <button
+            onClick={onRecovery}
+            className="ml-auto underline-offset-4 hover:underline text-xs md:text-sm"
+          >
+            Забыли пароль?
+          </button>
+        }
+      />
+    </AuthForm>
+  );
+};
+export default LoginForm;
