2f99b7da8c5e2707f7e2d3688dbf5589652b4a8d
sacazidal <tw1nkle001@yandex.ru>
2025-05-22 02:11:08 +0700

    валидация zod login

---
+++ frontend/lib/validations.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z
+    .string({ required_error: "Электронная почта обязательна к заполнению" })
+    .email("Введите корректный email"),
+  password: z
+    .string({ required_error: "Пароль обязателен к заполнению" })
+    .min(8, "Пароль должен содержать не менее 8 символов")
+    .regex(/[0-9]/, "Пароль должен содержать хотя бы одну цифру")
+    .regex(/[A-Z]/, "Пароль должен содержать хотя бы одну заглавную букву")
+    .regex(
+      /[@$!%*?&]/,
+      "Пароль должен содержать хотя бы один спецсимвол '@$!%*?&'"
+    ),
+});
+
+export type LoginSchema = z.infer<typeof loginSchema>;
