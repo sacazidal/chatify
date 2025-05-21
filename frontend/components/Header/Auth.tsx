6bd1fac3fd677c99705984e55d827eaff8514226
sacazidal <tw1nkle001@yandex.ru>
2025-05-21 23:36:55 +0700

    добавил модальное окно при клике, анимации

--- frontend/components/Header/Auth.tsx
+++ frontend/components/Header/Auth.tsx
@@ -3,19 +3,41 @@
 import { motion } from "framer-motion";
 import { User2Icon } from "lucide-react";
 import { Button } from "../ui/button";
+import AuthModal from "../Auth/AuthModal";
+import { useState } from "react";
+import { AuthModalType } from "@/types";
 
 const Auth = () => {
+  const [isAuthModalOpen, setIsAuthModalOpen] = useState<boolean>(false);
+  const [authModalType, setAuthModalType] = useState<AuthModalType>("login");
+
+  const handleClick = () => {
+    setIsAuthModalOpen(true);
+    setAuthModalType("login");
+  };
+
   return (
-    <motion.div
-      whileHover={{ scale: 1.03 }}
-      whileTap={{ scale: 0.99 }}
-      transition={{ type: "spring", stiffness: 400, damping: 10 }}
-    >
-      <Button className="flex gap-2 items-center cursor-pointer">
-        <User2Icon />
-        <p className="">Авторизация</p>
-      </Button>
-    </motion.div>
+    <>
+      <motion.div
+        whileHover={{ scale: 1.03 }}
+        whileTap={{ scale: 0.99 }}
+        transition={{ type: "spring", stiffness: 400, damping: 10 }}
+      >
+        <Button
+          onClick={handleClick}
+          className="flex gap-2 items-center cursor-pointer"
+        >
+          <User2Icon />
+          <p className="">Авторизация</p>
+        </Button>
+      </motion.div>
+
+      <AuthModal
+        isOpen={isAuthModalOpen}
+        initialForm={authModalType}
+        onClose={() => setIsAuthModalOpen(false)}
+      />
+    </>
   );
 };
 export default Auth;
