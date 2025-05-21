89525cc12c7ed9fadf47396c5755502bb0e18a60
sacazidal <tw1nkle001@yandex.ru>
2025-05-22 02:10:31 +0700

    добавил показать/скрыть пароль

--- frontend/components/Auth/LoginForm.tsx
+++ frontend/components/Auth/LoginForm.tsx
@@ -5,10 +5,12 @@ import AuthForm from "./AuthForm";
 import { SwitchFormProps } from "@/types";
 import FieldForm from "../FieldForm";
 import { API_URL } from "@/utils/route";
+import { EyeClosedIcon, EyeIcon } from "lucide-react";
 
 const LoginForm = ({ onSwitch, onRecovery }: SwitchFormProps) => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -65,21 +67,31 @@ const LoginForm = ({ onSwitch, onRecovery }: SwitchFormProps) => {
         id="email"
         placeholder="eye@example.com"
       />
-      <FieldForm
-        label="Пароль"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        type="password"
-        id="password"
-        recovery={
-          <button
-            onClick={onRecovery}
-            className="ml-auto underline-offset-4 hover:underline text-xs md:text-sm"
-          >
-            Забыли пароль?
-          </button>
-        }
-      />
+      <div className="relative">
+        <FieldForm
+          label="Пароль"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type={showPassword ? "password" : "text"}
+          id="password"
+          recovery={
+            <button
+              onClick={onRecovery}
+              className="ml-auto underline-offset-4 hover:underline text-xs md:text-sm"
+            >
+              Забыли пароль?
+            </button>
+          }
+        />
+        <button
+          type="button"
+          className="absolute right-3 top-[34px]"
+          onClick={() => setShowPassword((prev) => !prev)}
+          aria-label={showPassword ? "Скрыть пароль" : "Показать пароль"}
+        >
+          {showPassword ? <EyeClosedIcon size={18} /> : <EyeIcon size={18} />}
+        </button>
+      </div>
     </AuthForm>
   );
 };
